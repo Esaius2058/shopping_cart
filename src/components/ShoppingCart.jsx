b4991ef600d4d8e25cd6d7770820ac06b14483ce
Esaius2058 <wabwirewild@gmail.com>
2024-11-23 20:11:40 +0300

    Updated ShoppingCart to use context passed down from App.jsx

--- src/components/ShoppingCart.jsx
+++ src/components/ShoppingCart.jsx
@@ -1,28 +1,43 @@
-const Cart = ({cartList, cartCounter, cartTracker, handleChange}) => {
-    const total = cartList.reduce((sum, item) => sum + item.price, 0);
+import { useContext } from "react";
+import { ShopContext } from "./ShopContext";
 
-		const uniqueCartList = cartList.filter(
-			(item, index, self) => index === self.findIndex((t) => t.id === item.id)
-		);
+const Cart = () => {
+  const { cartList, cartCounter, cartTracker, handleChange, error } = useContext(ShopContext);
+  const total = cartList.reduce((sum, item) => sum + item.price, 0);
 
-    return (
-        <div className="cart-container">
-            <h2>Shopping Cart</h2>
-            {uniqueCartList.map((item) => (
-                <div key={item.id} className="cart-item">
-                    <img src={item.thumbnail} alt={item.title} />
-                    <div>
-                        <p>{item.title}</p>
-                        <p>{item.description}</p>
-                        <p>{item.stock} in stock</p>
-                        <input name="quantity-input" type="number" min="0" max={item.stock} value={cartTracker(item, cartList)} onChange={(e) => handleChange(e, item)}/>
-                    </div>
-                    <p>${item.price}</p>
-                </div>
-            ))}
-            <p>Subtotal ({cartCounter} items): {total.toFixed(2)}</p>
+  const uniqueCartList = cartList.filter(
+    (item, index, self) => index === self.findIndex((t) => t.id === item.id)
+  );
+
+  if (error) return <div>{error}</div>
+  
+  return (
+    <div className="cart-container">
+      <h2>Shopping Cart</h2>
+      {uniqueCartList.map((item) => (
+        <div key={item.id} className="cart-item">
+          <img src={item.thumbnail} alt={item.title} />
+          <div>
+            <p>{item.title}</p>
+            <p>{item.description}</p>
+            <p>{item.stock} in stock</p>
+            <input
+              name="quantity-input"
+              type="number"
+              min="0"
+              max={item.stock}
+              value={cartTracker(item, cartList)}
+              onChange={(e) => handleChange(e, item)}
+            />
+          </div>
+          <p>${item.price}</p>
         </div>
-    )
-}
+      ))}
+      <p>
+        Subtotal ({cartCounter} items): {total.toFixed(2)}
+      </p>
+    </div>
+  );
+};
 
 export default Cart;
