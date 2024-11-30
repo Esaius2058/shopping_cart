612f78762aec11a93d102a3fd3b7a840d5d94b99
Esaius2058 <wabwirewild@gmail.com>
2024-11-30 14:21:24 +0300

    Made HomePage a child of the / path

--- src/components/routes.jsx
+++ src/components/routes.jsx
@@ -4,38 +4,42 @@ import ErrorPage from "./ErrorPage";
 import Cart from "./ShoppingCart";
 import ProductList from "./Products";
 import Checkout from "./Checkout";
+import HomePage from "./Home";
 
 const MainRoutes = () => {
-	const routes = [
+  const routes = [
     {
-			path: "/home",
-			element: <App />,
-			errorElement: <ErrorPage />,
-			children: [
-				{
-					path: "shop",
-					element: <ShopWrapper />,
-					errorElement: <ErrorPage />,
-					children: [
-						{
-							path: "",
-							element: <ProductList />
-						},
-						{
-							path: "cart",
-							element: <Cart />
-						},
-						{
-							path: "checkout",
-							element: <Checkout />
-						}
-					]
-				}
-			]
-		}
-];
+      path: "/",
+      element: <App />,
+      errorElement: <ErrorPage />,
+      children: [
+        {
+          index:true,
+          element: <HomePage />,
+        },
+        {
+          path: "shop",
+          element: <ShopWrapper />,
+          children: [
+            {
+              path: "",
+              element: <ProductList />,
+            },
+            {
+              path: "cart",
+              element: <Cart />,
+            },
+            {
+              path: "checkout",
+              element: <Checkout />,
+            },
+          ],
+        },
+      ],
+    },
+  ];
 
-return routes;
+  return routes;
 };
 
 export default MainRoutes;
