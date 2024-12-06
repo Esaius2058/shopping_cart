73aeb6366822115a58af5140b56f56899b032efc
Esaius2058 <wabwirewild@gmail.com>
2024-12-06 23:15:13 +0300

    Added showMessage state to show when a user add an item to the cart

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -5,30 +5,36 @@ import HomeIcon from "../assets/home_icon";
 import ShoppingCartIcon from "../assets/shoppingcart_icon";
 
 const ProductList = () => {
-  const {products,cartCounter, message, addToCart} = useContext(ShopContext);
+  const { products, cartCounter, message, addToCart, showMessage } = useContext(ShopContext);
 
-  if (!products || products.length === 0){
-    return <div>Products are currently being restocked.</div>
+  if (!products || products.length === 0) {
+    return <div>Loading products...</div>;
   }
 
   return (
     <>
       <div className="products-header">
-        <Link to="/">
-          <HomeIcon />
-        </Link>
-        <Link to="cart">
-          <ShoppingCartIcon />
-        </Link>
+        <div className="home-icon">
+          <Link to="/">
+            <HomeIcon />
+          </Link>
+        </div>
+        <div>
+          <Link to="cart" className="cart-icon">
+            <ShoppingCartIcon />
+            <p>{cartCounter}</p>
+          </Link>
+        </div>
       </div>
-      <div className="cart-info">
-        <p>Cart Items: {cartCounter}</p>
-        <p>{message}</p>
+      {showMessage && (
+        <div className="cart-info">
+        {message}
       </div>
+      )}    
       <div className="products-container">
         {products.map((product) => (
-          <div key={product.id} className="grid-item-shop">
-            <button>
+          <div key={product.id} className="product-item">
+            <button className="grid-item-shop">
               <img src={product.thumbnail} alt={product} />
               <div className="price-title">
                 <p>{product.title}</p>
