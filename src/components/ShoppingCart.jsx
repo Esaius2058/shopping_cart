0a629d9e3012dfa8e1304ddbf9ac2e82f89eec7c
Esaius2058 <wabwirewild@gmail.com>
2024-12-06 23:18:10 +0300

    Added ShowMessage state to show when an item is added or deleted from the cart

--- src/components/ShoppingCart.jsx
+++ src/components/ShoppingCart.jsx
@@ -1,9 +1,18 @@
 import { useContext } from "react";
 import { ShopContext } from "./ShopContext";
 import { Link } from "react-router-dom";
+import HomeIcon from "../assets/home_icon";
 
 const Cart = () => {
-  const { cartList, cartCounter, cartTracker, handleChange} = useContext(ShopContext);
+  const {
+    cartList,
+    cartCounter,
+    cartTracker,
+    clearCart,
+    handleChange,
+    message,
+    showMessage,
+  } = useContext(ShopContext);
   const total = cartList.reduce((sum, item) => sum + item.price, 0);
 
   const uniqueCartList = cartList.filter(
@@ -11,31 +20,64 @@ const Cart = () => {
   );
 
   return (
-    <div className="cart-container">
-      <h2>Shopping Cart</h2>
-      <Link to="/shop">Shop</Link>
-      {uniqueCartList.map((item) => (
-        <div key={item.id} className="cart-item">
-          <img src={item.thumbnail} alt={item.title} />
-          <div>
-            <p>{item.title}</p>
-            <p>{item.description}</p>
-            <p>{item.stock} in stock</p>
-            <input
-              name="quantity-input"
-              type="number"
-              min="0"
-              max={item.stock}
-              value={cartTracker(item, cartList)}
-              onChange={(e) => handleChange(e, item)}
-            />
+    <div>
+      <div className="cart-header">
+        <div className="products-header">
+          <div className="home-icon">
+            <Link to="/">
+              <HomeIcon />
+            </Link>
           </div>
-          <p>${item.price}</p>
+          <div>
+            <h2>Shopping Cart</h2>
+          </div>
+          <div>
+            <Link to="/shop" className="cart-icon">
+              SHOP
+            </Link>
+          </div>
         </div>
-      ))}
-      <p>
-        Subtotal ({cartCounter} items): {total.toFixed(2)}
-      </p>
+      </div>
+      <div className="cart-container">
+        <div className="cart-items">
+          {uniqueCartList.map((item) => (
+            <div key={item.id} className="cart-item">
+              <div className="item-image">
+                <img src={item.thumbnail} alt={item.title} />
+              </div>
+              <div className="item-details">
+                <p>{item.title}</p>
+                <p>{item.stock} in stock</p>
+                <p>${item.price}</p>
+                <input
+                  name="quantity-input"
+                  type="number"
+                  min="0"
+                  max={item.stock}
+                  value={cartTracker(item, cartList)}
+                  onChange={(e) => handleChange(e, item)}
+                />
+              </div>
+            </div>
+          ))}
+        </div>
+        {showMessage && <div className="cart-info">{message}</div>}
+        <div className="cart-details">
+          <p>
+            Subtotal ({cartCounter} items): {total.toFixed(2)}
+          </p>
+          <div className="cart-buttons">
+            <button type="button" className="clear-cart" onClick={() => clearCart()}>
+              Clear Cart
+            </button>
+            <Link to="/shop/checkout">
+              <button className="checkout-button" type="button">
+                Checkout
+              </button>
+            </Link>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
