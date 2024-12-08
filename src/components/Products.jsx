972d1bbcbd56939881c99caef54e9e7d30ac7240
Esaius2058 <wabwirewild@gmail.com>
2024-12-08 11:07:11 +0300

    Added a product-categories division for filtering and easier navigation

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -5,19 +5,29 @@ import HomeIcon from "../assets/home_icon";
 import ShoppingCartIcon from "../assets/shoppingcart_icon";
 
 const ProductList = () => {
-  const { products, cartCounter, message, addToCart, showMessage } = useContext(ShopContext);
+  const {
+    products,
+    cartCounter,
+    message,
+    addToCart,
+    showMessage,
+    filterProductList,
+  } = useContext(ShopContext);
 
   if (!products || products.length === 0) {
     return <div>Loading products...</div>;
   }
 
   return (
-    <>
+    <div className="products-body">
       <div className="products-header">
         <div className="home-icon">
           <Link to="/">
             <HomeIcon />
           </Link>
+        </div>
+        <div className="products-title">
+          <h2>Shop</h2>
         </div>
         <div>
           <Link to="cart" className="cart-icon">
@@ -26,31 +36,43 @@ const ProductList = () => {
           </Link>
         </div>
       </div>
-      {showMessage && (
-        <div className="cart-info">
-        {message}
+      <div className="product-details">
+        <div className="product-categories">
+          <label htmlFor="categories">Categories</label>
+          <select
+            name="categories"
+            id="categories"
+            onChange={(e) => filterProductList(e.target.value)}
+          >
+            <option value="all">All Products</option>
+            <option value="beauty">Beauty Products</option>
+            <option value="fragrances">Fragrances</option>
+            <option value="furniture">Furniture</option>
+            <option value="groceries">Groceries</option>
+          </select>
+        </div>
+        <div className="products-container" id="products-container">
+          {products.map((product) => (
+            <div key={product.id} className="product-item">
+              <button className="grid-item-shop">
+                <img src={product.thumbnail} alt={product} />
+                <div className="price-title">
+                  <p>{product.title}</p>
+                  <p>${product.price}</p>
+                </div>
+              </button>
+              <button
+                className="grid-item-button"
+                onClick={() => addToCart(product)}
+              >
+                Add To Cart
+              </button>
+            </div>
+          ))}
+          {showMessage && <div className="cart-info">{message}</div>}
+        </div>
       </div>
-      )}    
-      <div className="products-container">
-        {products.map((product) => (
-          <div key={product.id} className="product-item">
-            <button className="grid-item-shop">
-              <img src={product.thumbnail} alt={product} />
-              <div className="price-title">
-                <p>{product.title}</p>
-                <p>${product.price}</p>
-              </div>
-            </button>
-            <button
-              className="add-to-cart-button"
-              onClick={() => addToCart(product)}
-            >
-              Add To Cart
-            </button>
-          </div>
-        ))}
-      </div>
-    </>
+    </div>
   );
 };
 export default ProductList;
