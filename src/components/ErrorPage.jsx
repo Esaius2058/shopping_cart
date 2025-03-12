08bef3b14b86b483aea5022ef33c6c2e8b920b05
Esaius2058 <wabwirewild@gmail.com>
2025-03-12 21:28:59 +0300

    update the error page

--- src/components/ErrorPage.jsx
+++ src/components/ErrorPage.jsx
@@ -1,12 +1,12 @@
 import { Link, useRouteError } from "react-router-dom";
 
-const ErrorPage = () => {
+const ErrorPage = (err) => {
   const error = useRouteError();
   return (
     <>
       <div>
         <h1>Something went wrong.</h1>
-        <div>{error?.message || "Page not found"}</div>
+        <div>{error?.message || err || "Page not found"}</div>
         <Link to="/">Back to home page.</Link>
       </div>
     </>
