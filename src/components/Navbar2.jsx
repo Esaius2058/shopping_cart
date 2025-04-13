bc79ef029a4384f1e1efee9501d95f040541ac18
Esaius2058 <wabwirewild@gmail.com>
2025-04-13 19:01:33 +0300

    customized navbar for the cart page

---
+++ src/components/Navbar2.jsx
@@ -0,0 +1,24 @@
+import ShoppingCartIcon from "../assets/shoppingcart_icon";
+import { useContext } from "react";
+import { ShopContext } from "./ShopContext";
+import { Link } from "react-router-dom";
+import Logo from "./Logo";
+
+const Navbar2 = () => {
+    const { cartCounter } = useContext(ShopContext);
+
+    return (
+        <div className="header">
+            <Logo />
+            <div className="nav-container">
+                <nav className="nav-bar">
+                    <Link to="/shop" className="link">
+                        SHOP
+                    </Link>
+                </nav>
+            </div>
+        </div>
+    );
+}
+
+export default Navbar2;
