6c56d85610e5472bea5336584668e597b4a5e1ae
Esaius2058 <wabwirewild@gmail.com>
2025-04-13 18:57:57 +0300

    removed the problematic div wrapping the products grid

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -2,6 +2,7 @@ import { useContext } from "react";
 import { ShopContext } from "./ShopContext";
 import { Link } from "react-router-dom";
 import ShoppingCartIcon from "../assets/shoppingcart_icon";
+import Logo from "./Logo";
 
 const ProductList = () => {
   const {
@@ -18,23 +19,10 @@ const ProductList = () => {
   }
 
   return (
-    <div className="products-body">
-      <div className="products-header">
-        {" "}
-        {/*Header*/}
-        <div className="title-container">
-          <Link to="/">
-            <img
-              src={"src/assets/house-laptop-solid.svg"}
-              alt="digero_logo"
-              className="digero_logo"
-            />
-          </Link>
-          <Link to="/" className="link-title">
-            <h1 className="home-title">DIGERO</h1>
-          </Link>
-        </div>
-        <div className="categories-cart">
+    <div className="shop">
+      <div className="shop-navigation">
+        <Logo />
+        <div className="shop-navigation-links">
           <div className="product-categories">
             <select
               name="categories"
@@ -56,29 +44,27 @@ const ProductList = () => {
           </div>
         </div>
       </div>
-      <div className="product-details">
-        <div className="products-container" id="products-container">
-          {products.map((product) => (
-            <div key={product.id} className="product-item">
-              <Link to={`/shop/${product.id}`}>
-                <button className="grid-item-shop">
-                  <img src={product.thumbnail} alt={product} />
-                  <div className="price-title">
-                    <p>{product.title}</p>
-                    <p>${product.price}</p>
-                  </div>
-                </button>
-              </Link>
-              <button
-                className="grid-item-button"
-                onClick={() => addToCart(product)}
-              >
-                Add To Cart
+      <div className="products-container" id="products-container">
+        {products.map((product) => (
+          <div key={product.id} className="product-item">
+            <Link to={`/shop/${product.id}`}>
+              <button className="grid-item-shop">
+                <img src={product.thumbnail} alt={product} />
+                <div className="price-title">
+                  <p>{product.title}</p>
+                  <p>${product.price}</p>
+                </div>
               </button>
-            </div>
-          ))}
-          {showMessage && <div className="cart-info">{message}</div>}
-        </div>
+            </Link>
+            <button
+              className="grid-item-button"
+              onClick={() => addToCart(product)}
+            >
+              Add To Cart
+            </button>
+          </div>
+        ))}
+        {showMessage && <div className="cart-info">{message}</div>}
       </div>
     </div>
   );
