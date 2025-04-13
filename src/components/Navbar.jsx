74990f38a7b5bf7b7d7231d7524de05641114ab3
Esaius2058 <wabwirewild@gmail.com>
2025-04-13 19:00:56 +0300

    default naviagtion bar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,28 @@
+import ShoppingCartIcon from "../assets/shoppingcart_icon";
+import { useContext } from "react";
+import { ShopContext } from "./ShopContext";
+import { Link } from "react-router-dom";
+import Logo from "./Logo";
+
+const Navbar = () => {
+    const { cartCounter } = useContext(ShopContext);
+
+    return (
+        <div className="header">
+            <Logo />
+            <div className="nav-container">
+                <nav className="nav-bar">
+                    <Link to="/shop" className="link">
+                        SHOP
+                    </Link>
+                    <Link to="/shop/cart" className="cart-icon">
+                        <ShoppingCartIcon />
+                        <p>{cartCounter}</p>
+                    </Link>
+                </nav>
+            </div>
+        </div>
+    );
+}
+
+export default Navbar;
