72290449fa5660f5bd8e081939cfd5552f49e09a
Esaius2058 <wabwirewild@gmail.com>
2025-04-14 00:26:38 +0300

    formatted file

--- src/components/ProductDescription.jsx
+++ src/components/ProductDescription.jsx
@@ -4,11 +4,11 @@ import { useParams, Link } from "react-router-dom";
 import HomeIcon from "../assets/home_icon";
 import ShoppingCartIcon from "../assets/shoppingcart_icon";
 import Navbar from "./Navbar";
-import starIcon from "../assets/star-solid.svg"; 
+import starIcon from "../assets/star-solid.svg";
 
 const Description = () => {
   const { productId } = useParams();
-  const { products, addToCart, cartCounter, message, showMessage} = useContext(ShopContext);
+  const { products, addToCart, cartCounter, message, showMessage } = useContext(ShopContext);
 
   const product = products.find((item) => item.id === parseInt(productId, 10));
 
@@ -20,35 +20,35 @@ const Description = () => {
     <div className="product-description">
       <Navbar />
       <div className="product-description-container">
-      <h3 className="product-description-title">{product.title}</h3>
-      <div className="product-description-image">
-        <img src={product.thumbnail} alt={product.title} />
-      </div>
-			<div className="product-description-about">
-        <h3>About this item</h3>
-        <p>{product.description}</p>
-      </div>
-      <div className="product-description-rating">
-        <p>
-          Rating: {product.rating}
-          <span>
-            <img src={starIcon} alt="star" className="rating-star"/>
-          </span>
-        </p>
-        <p>{product.shippingInformation}</p>
-        <p>{product.stock} left in stock</p>
-        <p>{product.returnPolicy || "No returns available"}</p>
-        <p>Warranty: {product.warranty || "No warranty provided"}</p>
-      </div>
-      <div className="product-description-buttons">
-        <button className="grid-item-button" onClick={() => addToCart(product)}>
-          Add To Cart
-        </button>
-        <Link to="/shop">
-          <button className="grid-item-button">Back to Shop</button>
-        </Link>
-      </div>
-			{showMessage && <div className="cart-info">{message}</div>}
+        <h3 className="product-description-title">{product.title}</h3>
+        <div className="product-description-image">
+          <img src={product.thumbnail} alt={product.title} />
+        </div>
+        <div className="product-description-about">
+          <h3>About this item</h3>
+          <p>{product.description}</p>
+        </div>
+        <div className="product-description-rating">
+          <p>
+            Rating: {product.rating}
+            <span>
+              <img src={starIcon} alt="star" className="rating-star" />
+            </span>
+          </p>
+          <p>{product.shippingInformation}</p>
+          <p>{product.stock} left in stock</p>
+          <p>{product.returnPolicy || "No returns available"}</p>
+          <p>Warranty: {product.warranty || "No warranty provided"}</p>
+        </div>
+        <div className="product-description-buttons">
+          <button className="grid-item-button" onClick={() => addToCart(product)}>
+            Add To Cart
+          </button>
+          <Link to="/shop">
+            <button className="grid-item-button">Back to Shop</button>
+          </Link>
+        </div>
+        {showMessage && <div className="cart-info">{message}</div>}
       </div>
     </div>
   );
